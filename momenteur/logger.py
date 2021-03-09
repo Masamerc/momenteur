2ec444c86042c7a34009c5d703cc64a8f822ac63
Masa Fukui <fkmkfkmasum41@gmail.com>
2021-03-09 14:58:24 +0900

    Add momenteur/logger.py

---
+++ momenteur/logger.py
@@ -0,0 +1,18 @@
+import logging
+
+logger = logging.getLogger('momenteur_backend')
+logger.setLevel(logging.DEBUG)
+
+# file / console handler & formatter
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+fh = logging.FileHandler('momenteur.log')
+fh = setLevel(logging.DEBUG)
+
+f = logging.Formatter('%(asctime)s - %(name)s - [%(levelname)s] - %(message)s', datefmt='%m/%d/%Y %H:%M:%S')
+ch.setFormatter(f)
+fh.setFormatter(f)
+
+logger.addHandler(ch)
+logger.addHandler(fh)
+
