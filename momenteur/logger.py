11676007a2ba6143211dcf06b895d7abb97b3f37
Masa Fukui <fkmkfkmasum41@gmail.com>
2021-03-09 15:15:29 +0900

    Update logger.py: Make FileHandler optional

--- momenteur/logger.py
+++ momenteur/logger.py
@@ -1,21 +1,24 @@
 import logging
 
+
 logger = logging.getLogger('momenteur_backend')
 logger.setLevel(logging.DEBUG)
 
-# file / console handler & formatter
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-fh = logging.FileHandler('/tmp/momenteur.log')
-fh.setLevel(logging.DEBUG)
-
 f = logging.Formatter('%(asctime)s.%(msecs)03d - %(name)s - [%(levelname)s] - %(message)s',
                       datefmt='%m/%d/%Y %H:%M:%S')
+
+# console handler
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
 ch.setFormatter(f)
-fh.setFormatter(f)
 
 logger.addHandler(ch)
-logger.addHandler(fh)
+
+## file handler (optional)
+# fh = logging.FileHandler('/tmp/momenteur.log')
+# fh.setLevel(logging.DEBUG)
+# fh.setFormatter(f)
+# logger.addHandler(fh)
 
 if __name__=='__main__':
     logger.info('info here')
