2ad790ba704e03c028d09bc8aa00f2c04a8afb14
Masa Fukui <fkmkfkmasum41@gmail.com>
2021-03-09 15:09:57 +0900

    Update logger.py: now timestamp shows up to milliseconds

--- momenteur/logger.py
+++ momenteur/logger.py
@@ -9,9 +9,15 @@ ch.setLevel(logging.DEBUG)
 fh = logging.FileHandler('/tmp/momenteur.log')
 fh.setLevel(logging.DEBUG)
 
-f = logging.Formatter('%(asctime)s - %(name)s - [%(levelname)s] - %(message)s', datefmt='%m/%d/%Y %H:%M:%S')
+f = logging.Formatter('%(asctime)s.%(msecs)03d - %(name)s - [%(levelname)s] - %(message)s',
+                      datefmt='%m/%d/%Y %H:%M:%S')
 ch.setFormatter(f)
 fh.setFormatter(f)
 
 logger.addHandler(ch)
 logger.addHandler(fh)
+
+if __name__=='__main__':
+    logger.info('info here')
+    logger.debug('debugging')
+    logger.warning('warning sign')
